7e8e0dd19d7883859036021a8bf1bf5388061175
builder555 <85308587+builder555@users.noreply.github.com>
2023-12-31 20:38:03 -0500

    make exceptions available

--- src/pinecil/__init__.py
+++ src/pinecil/__init__.py
@@ -4,4 +4,9 @@ from .pinecil import (
     ValueOutOfRangeException,
     InvalidSettingException,
 )
-from .ble import find_device_addresses, BLE
+from .ble import (
+    BLE,
+    find_device_addresses,
+    DeviceNotFoundException,
+    DeviceDisconnectedException,
+)
